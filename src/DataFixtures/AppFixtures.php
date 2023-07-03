32e991f1c689e2553bc5c7026df89d930db5503a
Gumbraise <vikiclashonyt@gmail.com>
2023-07-03 14:04:36 +0200

    Added Fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Etat;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        foreach (['Créée', 'Ouverte', 'Clôturée', 'Activité en cours', 'Passée', 'Annulée'] as $libelle) {
+            $etat = new Etat();
+            $etat->setLibelle($libelle);
+            $manager->persist($etat);
+        }
+
+        $manager->flush();
+    }
+}
