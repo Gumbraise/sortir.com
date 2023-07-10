1f22f38a204310cc9474eff0e1ba408277c02b93
Gumbraise <vikiclashonyt@gmail.com>
2023-07-10 11:29:36 +0200

    Improved 0547afb

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,14 +12,6 @@ class HomepageController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
-        if($this->getUser() != null){
-            $user = $this->getUser();
-            if(!$user->isActif()){
-                $this->addFlash('success', 'Erreur : Votre compte à été désactivé, veuillez contacter un responsable.');
-                return $this->redirectToRoute('app_logout');
-            }
-        }
-        return $this->render('homepage/index.html.twig', [
-        ]);
+        return $this->render('homepage/index.html.twig');
     }
 }
