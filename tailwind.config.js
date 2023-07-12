1bd4a25baa73cd9c6bbb5557a0414fd9fa9e23ce
Gumbraise <vikiclashonyt@gmail.com>
2023-07-12 13:35:14 +0200

    Fixed tailwind compile bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
